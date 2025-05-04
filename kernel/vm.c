7c3e1b7b75145d7db33e0bc89604258696c846b2
ycb <ycb@Hder.com>
2025-05-04 13:33:25 +0800

    Lab6 finished

--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -311,7 +313,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+  //char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -319,14 +321,25 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+
+    // if the page is writable, set it to copy-on-write.
+    if (*pte & PTE_W)
+      *pte = (*pte & ~PTE_W) | PTE_COW;
+    
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
+
+    if (mappages(new, i, PGSIZE, pa, flags) != 0)
+      goto err;
+    
+    kama_krefpage((void*)pa);
+
+    /*if((mem = kalloc()) == 0)
       goto err;
     memmove(mem, (char*)pa, PGSIZE);
     if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
       kfree(mem);
       goto err;
-    }
+    }*/
   }
   return 0;
 
@@ -348,6 +361,38 @@ uvmclear(pagetable_t pagetable, uint64 va)
   *pte &= ~PTE_U;
 }
 
+int
+kama_uvmcheckcowpage(uint64 va) {
+  pte_t* pte;
+  struct proc* p = myproc();
+
+  return va < p->sz
+      && ((pte = walk(p->pagetable, va, 0)) != 0)
+      && (*pte & PTE_V)
+      && (*pte & PTE_COW);
+}
+
+// COW
+int
+kama_uvmcowcopy(uint64 va) {
+  pte_t* pte;
+  struct proc* p = myproc();
+  if ((pte = walk(p->pagetable, va, 0)) == 0)
+    panic("uvmcowcopy: walk");
+  
+  uint64 pa = PTE2PA(*pte);
+  uint64 new = (uint64)kama_kcopy_n_deref((void*)pa);
+  if (new == 0)
+    return -1;
+
+  uint64 flags = (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW;
+  uvmunmap(p->pagetable, PGROUNDDOWN(va), 1, 0);
+  if (mappages(p->pagetable, va, 1, new, flags) == -1)
+    panic("uvmcowcopy: mappages");
+  
+  return 0;
+}
+
 // Copy from kernel to user.
 // Copy len bytes from src to virtual address dstva in a given page table.
 // Return 0 on success, -1 on error.
@@ -357,6 +402,10 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
   uint64 n, va0, pa0;
 
   while(len > 0){
+
+    if (kama_uvmcheckcowpage(dstva))
+      kama_uvmcowcopy(dstva);
+
     va0 = PGROUNDDOWN(dstva);
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
